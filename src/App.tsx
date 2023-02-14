65055cc81e68bae1f724c0a0dc6b8c2b69581ac0
Shimi Zacken <shimizacken@gmail.com>
2023-02-14 18:19:36 +0100

    chore: pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 
 import './App.scss';
+import { Weather } from './components/pages/Weather.page';
 
 function App() {
   return (
     <div className="App">
+      <Weather />
     </div>
   );
 }
---
+++ src/components/pages/Weather.page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Weather: React.FC = () => <div>
+    Weather
+</div>;
