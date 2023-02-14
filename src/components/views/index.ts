0efb61a797c3b767c4314b4f6c8b7fc1fc1c9861
Shimi Zacken <shimizacken@gmail.com>
2023-02-14 22:47:35 +0100

    chore: exported

--- src/components/views/index.ts
+++ src/components/views/index.ts
@@ -4,3 +4,6 @@ export {ButtonType} from './elements/Button/buttonType';
 export {Header} from './elements/Header/Header.view';
 export {Footer} from './elements/Footer/Footer.view';
 export {Text} from './elements/Text/Text.view';
+
+// modules
+export {WeatherCard} from './modules/WeatherCard/WeatherCard.view';
