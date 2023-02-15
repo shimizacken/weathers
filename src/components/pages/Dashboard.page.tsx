5ef508799b9285eac896b7a9539196bbd711f79c
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 13:13:01 +0100

    chore: additional info integrated

--- src/components/pages/Dashboard.page.tsx
+++ src/components/pages/Dashboard.page.tsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import {WeatherCard} from '../views';
 
-export const Dashboard: React.FC = () => <article>Dashboard</article>;
+export const Dashboard: React.FC = () => (
+    <article>
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+        <WeatherCard locationName="Berlin" currentTemperature="20C°" />
+    </article>
+);
--- src/components/views/modules/DetailedWeather/DetailedWeather.view.tsx
+++ src/components/views/modules/DetailedWeather/DetailedWeather.view.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import {Text} from '../../Foundation/Text/Text.view';
+import {AdditionalInfo} from '../AdditionalInfo/AdditionalInfo.view';
+import styles from './DetailedWeather.module.scss';
 
 export const DetailedWeather: React.FC<{currentTemperature: string}> = ({
     currentTemperature,
 }) => {
     return (
-        <article>
-            <section className="">
+        <article className={styles.wrapper}>
+            <section>
                 <Text size="huge">{currentTemperature}</Text>
             </section>
-            <section></section>
+            <section className={styles.additionalInfoSection}>
+                <div>
+                    <AdditionalInfo
+                        name="Sunrise"
+                        value="05:40"
+                        className={styles.additionalInfo}
+                    />
+                    <AdditionalInfo
+                        name="Sunset"
+                        value="20:26"
+                        className={styles.additionalInfo}
+                    />
+                </div>
+                <div>
+                    <AdditionalInfo
+                        name="Humidity"
+                        value="26%"
+                        className={styles.additionalInfo}
+                    />
+                    <AdditionalInfo
+                        name="Visibility"
+                        value="16.2km"
+                        className={styles.additionalInfo}
+                    />
+                </div>
+            </section>
         </article>
     );
 };
