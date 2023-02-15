cdb54d0ac59cb7de29e5f451400c8b00ba325b08
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 17:30:09 +0100

    chore: add card

--- src/components/pages/Dashboard.page.tsx
+++ src/components/pages/Dashboard.page.tsx
@@ -3,6 +3,7 @@ import {WeatherCardContainer} from '../viewModels/WeatherCard.viewModel';
 
 export const Dashboard: React.FC = () => (
     <article>
+        <WeatherCardContainer locationName="Oslo" />
         <WeatherCardContainer locationName="Berlin" />
         <WeatherCardContainer locationName="London" />
     </article>
