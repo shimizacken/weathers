8dda53b37bb56256274709924055c42949eff6fd
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 23:01:16 +0100

    style: header flexed

--- src/components/views/elements/Header/Header.view.tsx
+++ src/components/views/elements/Header/Header.view.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Header.module.scss';
 
 export const Header: React.FC<{
     start?: React.ReactNode;
     middle?: React.ReactNode;
     end?: React.ReactNode;
 }> = ({start, middle, end}) => (
-    <header>
+    <header className={styles.header}>
         <section>{start}</section>
         <section>{middle}</section>
         <section>{end}</section>
