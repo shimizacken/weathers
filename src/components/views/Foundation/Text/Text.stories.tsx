c1cdd49a80b25794ed7472680eb302427ca85793
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 12:48:08 +0100

    chore: more stories

--- src/components/views/Foundation/Text/Text.stories.tsx
+++ src/components/views/Foundation/Text/Text.stories.tsx
@@ -11,9 +11,34 @@ export default {
 
 const Template: ComponentStory<typeof Text> = args => <Text {...args} />;
 
-export const Primary = Template.bind({});
+export const Medium = Template.bind({});
+export const Small = Template.bind({});
+export const Large = Template.bind({});
+export const Huge = Template.bind({});
+export const Bold = Template.bind({});
 
-Primary.args = {
+Medium.args = {
     children: 'Some text',
     size: 'medium',
+};
+
+Small.args = {
+    ...Medium.args,
+    size: 'small',
+};
+
+Large.args = {
+    ...Medium.args,
+    size: 'large',
+};
+
+Huge.args = {
+    ...Medium.args,
+    size: 'huge',
+};
+
+Bold.args = {
+    ...Medium.args,
+    size: 'medium',
+    weight: 'bold',
 };
