ff63f38de80aa702ad6b7c55a9bdc0d7f030657f
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 20:28:14 +0100

    chore: icon added

--- src/components/views/modules/DetailedWeather/DetailedWeather.view.tsx
+++ src/components/views/modules/DetailedWeather/DetailedWeather.view.tsx
@@ -1,43 +1,52 @@
 import React from 'react';
 import type {WeatherResult} from '../../../../types/types';
+import {buildIconUrl} from '../../../../utils/openWeatherMap/api';
 import {Text} from '../../Foundation/Text/Text.view';
 import {AdditionalInfo} from '../AdditionalInfo/AdditionalInfo.view';
 import styles from './DetailedWeather.module.scss';
 
 export const DetailedWeather: React.FC<{weather?: WeatherResult}> = ({
     weather,
-}) => {
-    return (
-        <article className={styles.wrapper}>
-            <section>
-                <Text size="huge">{weather?.displayCurrentTemperature}</Text>
-            </section>
-            <section className={styles.additionalInfoSection}>
-                <div>
-                    <AdditionalInfo
-                        name="Sunrise"
-                        value={weather?.sunrise ?? ''}
-                        className={styles.additionalInfo}
+}) => (
+    <article className={styles.wrapper}>
+        <section>
+            <div className={styles.temperature}>
+                <Text size="medium">{weather?.description}</Text>{' '}
+                {weather?.icon && (
+                    <img
+                        src={buildIconUrl(weather.icon)}
+                        title={weather?.description}
+                        alt={weather?.description}
                     />
-                    <AdditionalInfo
-                        name="Sunset"
-                        value={weather?.sunset ?? ''}
-                        className={styles.additionalInfo}
-                    />
-                </div>
-                <div>
-                    <AdditionalInfo
-                        name="Humidity"
-                        value={weather?.humidity ?? ''}
-                        className={styles.additionalInfo}
-                    />
-                    <AdditionalInfo
-                        name="Visibility"
-                        value={weather?.visibility ?? ''}
-                        className={styles.additionalInfo}
-                    />
-                </div>
-            </section>
-        </article>
-    );
-};
+                )}
+            </div>
+            <Text size="huge">{weather?.displayCurrentTemperature}</Text>
+        </section>
+        <section className={styles.additionalInfoSection}>
+            <div>
+                <AdditionalInfo
+                    name="Sunrise"
+                    value={weather?.sunrise ?? ''}
+                    className={styles.additionalInfo}
+                />
+                <AdditionalInfo
+                    name="Sunset"
+                    value={weather?.sunset ?? ''}
+                    className={styles.additionalInfo}
+                />
+            </div>
+            <div>
+                <AdditionalInfo
+                    name="Humidity"
+                    value={weather?.humidity ?? ''}
+                    className={styles.additionalInfo}
+                />
+                <AdditionalInfo
+                    name="Visibility"
+                    value={weather?.visibility ?? ''}
+                    className={styles.additionalInfo}
+                />
+            </div>
+        </section>
+    </article>
+);
