101399b32ff8cd7fadd5f2cbb6c8138c2d7dc783
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 22:48:26 +0100

    chore: integrated

--- src/components/pages/Dashboard.page.tsx
+++ src/components/pages/Dashboard.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {PersonalWeatherCard} from '../viewModels/PersonalWeatherCard.viewModel';
 import {WeatherCardContainer} from '../viewModels/WeatherCard.viewModel';
 import {PageContent, WeatherCards} from '../views';
 
 export const Dashboard: React.FC = () => (
     <PageContent>
         <WeatherCards>
+            <PersonalWeatherCard />
             <WeatherCardContainer locationName="New York" />
             <WeatherCardContainer locationName="Berlin" />
             <WeatherCardContainer locationName="London" />
