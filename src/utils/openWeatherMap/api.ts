a2e55a15bf4f43bdc07c371d31bb5c8ef3d7e03d
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 14:16:49 +0100

    feat: api utils added

---
+++ src/utils/openWeatherMap/api.ts
@@ -0,0 +1,41 @@
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=';
+
+export const addUnit = (unit: string) => (unit ? `&units=${unit}` : '');
+
+export const buildBaseTokenUrl = (token: string, unit: string) =>
+    `${baseUrl}${token}${unit}`;
+
+export const buildFetchWeatherBaseURL = (token: string) => (unit: string) => {
+    const metricType = addUnit(unit);
+    const baseTokenUrl = buildBaseTokenUrl(token, metricType);
+
+    return (queryStringParams: Record<string, string>) => {
+        let url = '';
+
+        Object.keys(queryStringParams).forEach((key: string) => {
+            url += `&${key}=${encodeURIComponent(queryStringParams[key])}`;
+        });
+
+        return `${baseTokenUrl}${url}`;
+    };
+};
+
+export const buildFetchWeatherByCityName = (token: string, unit: string) => {
+    unit = unit ? `&units=${unit}` : '';
+
+    return (cityName: string) =>
+        `${baseUrl}${token}${unit}&q=${encodeURIComponent(cityName)}`;
+};
+
+export const buildFetchWeatherByGeographicCoordinates = (
+    token: string,
+    unit: string
+) => {
+    unit = unit ? `&units=${unit}` : '';
+
+    return (lat: string, lon: string) =>
+        `${baseUrl}${token}${unit}&lat=${lat}&lon=${lon}`;
+};
+
+export const buildIconUrl = (icon: string) =>
+    `http://openweathermap.org/img/w/${icon}.png`;
