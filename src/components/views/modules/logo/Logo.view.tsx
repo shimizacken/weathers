f114551260859e5e5563862ff343a73bdb6d6c4b
Shimi Zacken <shimizacken@gmail.com>
2023-02-15 19:06:23 +0100

    style: media query added

--- src/components/views/modules/logo/Logo.view.tsx
+++ src/components/views/modules/logo/Logo.view.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import cx from 'classnames';
 import sun from '../../../../assets/wf-logo.png';
+import styles from './Logo.module.scss';
 
 export const Logo: React.FC<{className?: string}> = ({className}) => (
-    <img className={className} src={sun} alt="Site logo" width={150} />
+    <img
+        className={cx(styles.logo, className)}
+        src={sun}
+        alt="Site logo"
+        width={150}
+    />
 );
