e4b0e8ab6c920478f7d18e923d8cde308772bbc8
Shimi Zacken <shimizacken@gmail.com>
2023-02-16 21:11:43 +0100

    feat: neutron added

---
+++ src/packages/neutron/neutron.ts
@@ -0,0 +1,65 @@
+import {Watcher, Neutron, NeutronType} from './types';
+
+/**
+ * Neutron observer
+ */
+export const neutron =
+    <T>(previousState?: T, emitted = false) =>
+    (behavior: NeutronType = NeutronType.Default): Neutron<T> => {
+        const watchers = new Set<Watcher<T>>();
+
+        /**
+         * unsubscribes from current neutron
+         */
+        const abandon = (watcherToRemove: Watcher<T>) =>
+            watchers.delete(watcherToRemove);
+
+        /**
+         * subscribes to current neutron
+         */
+        const watch = (watcher: Watcher<T>) => {
+            watchers.add(watcher);
+
+            if (behavior === NeutronType.ReEmit && emitted === true) {
+                emitToSingleWatcher(watcher, previousState);
+            }
+
+            return () => abandon(watcher);
+        };
+
+        const emitToSingleWatcher = (
+            watcher: Watcher<T>,
+            nextState?: T,
+            previousState?: T
+        ) => watcher(nextState, previousState);
+
+        /**
+         * fires new data to all observers
+         */
+        const emit = (nextState?: T) => {
+            watchers.forEach(watcher =>
+                emitToSingleWatcher(watcher, nextState, previousState)
+            );
+
+            previousState = nextState;
+            emitted = true;
+        };
+
+        /**
+         * return array of current watchers
+         */
+        const getWatchers = () => Array.from(watchers);
+
+        return {
+            watch,
+            abandon,
+            emit,
+            getWatchers,
+        };
+    };
+
+/**
+ * create a Neutron observer
+ */
+export const createNeutron = <T>(behavior?: NeutronType): Neutron<T> =>
+    neutron<T>()(behavior);
---
+++ src/packages/neutron/types.ts
@@ -0,0 +1,19 @@
+export type Watcher<T> = (next?: T, previous?: T) => void;
+
+export enum NeutronType {
+    /**
+     * emit only when explicitly called
+     */
+    Default = 'default',
+    /**
+     * always re-emit latest state to any new watcher
+     */
+    ReEmit = 're-emit-for-new-watcher',
+}
+
+export interface Neutron<T> {
+    watch: (watcher: Watcher<T>) => () => boolean;
+    abandon: (watcher: Watcher<T>) => boolean;
+    emit: (nextState?: T) => void;
+    getWatchers: () => Array<Watcher<T>>;
+}
