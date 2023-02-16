78b4ce8c77bc9ad3edf95ed56fedb1cc08af4d34
Shimi Zacken <shimizacken@gmail.com>
2023-02-16 21:42:48 +0100

    chore: signal added

--- src/components/viewModels/MetricContainer.viewModel.tsx
+++ src/components/viewModels/MetricContainer.viewModel.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Units} from '../../utils';
+import {unitSignal} from '../../signals/unit.signals';
+import {getUnitNameByValue, Units} from '../../utils';
 import {MetricRadioButtons} from '../views/modules/MetricRadioButtons/MetricRadioButtons';
 
 export const MetricContainer: React.FC = () => {
     const radioChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(
-            '🚀 ~ file: MetricContainer.viewModel.tsx:8 ~ radioChanged ~ e.target.value',
-            e.target.value
-        );
-        // const unit = Units[e.target.value];
+        const unit = getUnitNameByValue(e.target.value);
+
+        localStorage.setItem('_unit_', unit.value);
+        unitSignal.emit(unit.value);
     };
 
-    return <MetricRadioButtons radioChanged={radioChanged} />;
+    return (
+        <MetricRadioButtons
+            radioChanged={radioChanged}
+            checked={localStorage.getItem('_unit_') ?? Units.celsius.value}
+        />
+    );
 };
