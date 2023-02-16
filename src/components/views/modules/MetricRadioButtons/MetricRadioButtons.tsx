713baf7314b7dc63ccc7d4e5b4fe63646bb6a826
Shimi Zacken <shimizacken@gmail.com>
2023-02-16 20:48:51 +0100

    chore: default unit

--- src/components/views/modules/MetricRadioButtons/MetricRadioButtons.tsx
+++ src/components/views/modules/MetricRadioButtons/MetricRadioButtons.tsx
@@ -5,32 +5,26 @@ import styles from './MetricRadioButtons.module.scss';
 
 export const MetricRadioButtons: React.FC<{
     radioChanged: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}> = ({radioChanged, ...props}) => (
+    checked: string;
+}> = ({radioChanged, checked, ...props}) => (
     <div className={styles.wrapper} {...props}>
         <RadioButton
             text="Celsius"
-            value={Units.celsius.name}
+            value={Units.celsius.value}
             name="temperatureType"
             className={styles.radioButton}
-            checked
             onChange={radioChanged}
             disabled={false}
+            checked={Units.celsius.value === checked}
         />
         <RadioButton
             text="Fahrenheit"
-            value={Units.fahrenheit.name}
+            value={Units.fahrenheit.value}
             name="temperatureType"
             className={styles.radioButton}
             onChange={radioChanged}
             disabled={false}
-        />
-        <RadioButton
-            text="Kelvin"
-            value={Units.kelvin.name}
-            name="temperatureType"
-            className={styles.radioButton}
-            onChange={radioChanged}
-            disabled={false}
+            checked={Units.fahrenheit.value === checked}
         />
     </div>
 );
