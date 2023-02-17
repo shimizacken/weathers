3221eda05f298a0cbedeb95f74158da07960406f
Shimi Zacken <shimizacken@gmail.com>
2023-02-17 11:32:24 +0100

    test: add test

--- cypress/e2e/startup.cy.ts
+++ cypress/e2e/startup.cy.ts
@@ -1,3 +1,11 @@
-it('works!', () => {
+import {TestId} from '../../src/testId';
+
+it('should load app', () => {
     cy.visit('http://localhost:3000/');
+});
+
+it('should have weather cards', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get(`[data-cy=${TestId.WEATHER_CARD}]`);
 });
